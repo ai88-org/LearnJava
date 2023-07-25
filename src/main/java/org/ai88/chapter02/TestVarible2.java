6a64761c4144405440d3010af8ff8167335c66cc
xuyang <1006351809@qq.com>
2023-07-25 14:09:18 +0800

    JAVA 学习第二章 浮点型变量

---
+++ src/main/java/org/ai88/chapter02/TestVarible2.java
@@ -0,0 +1,9 @@
+package org.ai88.chapter02;
+
+public class TestVarible2 {
+    public static void main(String[] args) {
+        double d1 = 0.2;
+        double d2 = 0.1;
+        System.out.println(d1+d2); // 输出 0.30000000000000004 不是0.3
+    }
+}
