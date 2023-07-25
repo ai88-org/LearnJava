0b808fc63d6c3f69592ffc8ff942030ff9a5038c
xuyang <1006351809@qq.com>
2023-07-25 11:34:10 +0800

    JAVA 学习第二章 整型变量

---
+++ src/main/java/org/ai88/chapter02/TestVariable1.java
@@ -0,0 +1,16 @@
+package org.ai88.chapter02;
+
+/**
+ * 整型基本数据类型的使用
+ */
+public class TestVariable1 {
+    public static void main(String[] args) {
+        byte b1 = 12;
+        byte b2 = 127;
+        // byte b3 = 128; 编译不通过，超过范围
+
+        int i1 = 128;
+        long l1 = 123544L;
+        long l2 = 123544l;
+    }
+}
