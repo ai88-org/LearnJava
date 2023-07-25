4c3a8441d4672874cbe666d0444fbb801fa2c856
xuyang <1006351809@qq.com>
2023-07-25 10:40:46 +0800

    JAVA 学习第一章

---
+++ readme.md
@@ -0,0 +1,4 @@
+# JAVA 基础
+
+## 第一章 Hello World
+
---
+++ src/main/java/org/ai88/chapter01/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.ai88.chapter01;
+
+/**
+ * 初学JAVA的第一个程序
+ * 输出Hello World
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好，世界！");
+        System.out.println("Hello World!");
+    }
+}
