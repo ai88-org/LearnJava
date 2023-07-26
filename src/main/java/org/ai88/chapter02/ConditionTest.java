15456a8f0233049c5932974b3707dca9e9fa4786
xuyang <1006351809@qq.com>
2023-07-26 16:05:10 +0800

    JAVA 学习第二章 条件运算符

---
+++ src/main/java/org/ai88/chapter02/ConditionTest.java
@@ -0,0 +1,20 @@
+package org.ai88.chapter02;
+
+public class ConditionTest {
+    public static void main(String[] args) {
+        double a = (2>1)? 1:12.3;
+        System.out.println(a);
+
+        // 获取两个数的最大值
+        int m = 12;
+        int n = 20;
+        System.out.println((m>n)?m:n);
+
+        // 获取三个数的最大值
+        int o = 30;
+
+        /*short s1 = 1;
+        s1 += 1;
+        System.out.println(s1);*/
+    }
+}
