3f8cd1ca56def31fe6696fb97ecb8cfe98af352a
xuyang <1006351809@qq.com>
2023-07-26 14:13:50 +0800

    JAVA 学习第二章 位运算符的练习

---
+++ src/main/java/org/ai88/chapter02/BitTest.java
@@ -0,0 +1,10 @@
+package org.ai88.chapter02;
+
+public class BitTest {
+    public static void main(String[] args) {
+        int num = 7;
+        System.out.println("<< 1 位："+(num << 1)); // 14
+        System.out.println("<< 2 位："+(num << 2)); // 28
+        System.out.println("<< 3 位："+(num << 3)); // 56
+    }
+}
