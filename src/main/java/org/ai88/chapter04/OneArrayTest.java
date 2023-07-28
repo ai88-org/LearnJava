f39513f320a2641ae5f6292cf6fb3b68e854a0db
xuyang <1006351809@qq.com>
2023-07-28 17:59:40 +0800

    JAVA 学习第四章 数组部分

---
+++ src/main/java/org/ai88/chapter04/OneArrayTest.java
@@ -0,0 +1,8 @@
+package org.ai88.chapter04;
+
+public class OneArrayTest {
+    public static void main(String[] args) {
+        String[] arr = new String[2];
+        String[] arr1 = {"a","b"};
+    }
+}
