9e997634eb49efb97d13ade6c1466ff026500944
xuyang <xuyang@unitechs.com>
2023-08-01 22:10:28 +0800

    方法初识

--- src/main/java/org/ai88/chapter05/Employee.java
+++ src/main/java/org/ai88/chapter05/Employee.java
@@ -6,4 +6,8 @@ public class Employee {
     int age;
     double salary;
     MyDate myDate;
+
+    public void show(){
+        System.out.println("编号是："+this.no+"，姓名是："+this.name+"，工资是："+this.salary);
+    }
 }
